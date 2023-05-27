23fb139da9d374b60aeffe567b7951af6f5c1ccc
Achmad Maulana <104055741+achmadmaulana162@users.noreply.github.com>
2023-05-27 19:10:47 +0700

    Update home.php

--- home.php
+++ home.php
@@ -1,4 +1,32 @@
- <!-- ======= Hero Section ======= -->
+<!-- ======= Header ======= -->
+ <header id="header" class="fixed-top">
+    <div class="container-fluid d-flex justify-content-between align-items-center">
+
+      <h4 class="logo me-auto me-lg-0"><a href="index.php?hal=home">Achmad</a></h4>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a class="active" href="index.php?hal=home">Home</a></li>
+          <li><a href="index.php?hal=about">About</a></li>
+          <li><a href="index.php?hal=resume">Resume</a></li>
+          <li><a href="index.php?hal=portfolio">Portfolio</a></li>
+          <li><a href="index.php?hal=skill">Skill</a></li>
+          <li><a href="index.php?hal=contact">Contact</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+      <div class="header-social-links">
+        <a href="https://www.instagram.com/ahmadmaulana10_/?hl=id" class="instagram"><i class="bi bi-instagram"></i></a>
+        <a href="#" class="linkedin"><i class="bi bi-github"></i>
+      </div>
+
+    </div>
+
+  </header><!-- End Header -->
+<!-- ======= Hero Section ======= -->
  <section id="hero" class="d-flex align-items-center">
     <div class="container d-flex flex-column align-items-center" data-aos="zoom-in" data-aos-delay="100">
       <h1>Achmad Maulana</h1>
